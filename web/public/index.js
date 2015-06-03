2e0c80818ec0f93efffe6e471eaf2e3b14aaabca
Hack Reactor Students <accounts@hackreactor.com>
2015-06-03 15:32:59 -0700

    Client able to send requests, server able to see them

---
+++ web/public/index.js
@@ -0,0 +1,31 @@
+//ajax request
+var apiURL = 'http://127.0.0.1:8080/websites';
+
+var sendRequest = function(website) {
+  $.ajax({
+    url: apiURL,
+    type: 'POST',
+    data: JSON.stringify(website),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('success');
+    },
+    error: function(xhr, textStatus, error) {
+      console.log(textStatus);
+    }
+  });
+};
+
+
+
+
+$(document).ready(function(){
+
+  $('#input-form').on('submit', function(e) {
+    e.preventDefault();
+    var website = {url: $('#url').val()};
+    sendRequest(website);
+  });
+
+
+});
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,51 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10,
+  'Content-Type': "application/json" // Seconds.
+};
+
+var sendResponse = function(response, statusCode) {
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode, headers);
+  response.end();
+};
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
+  console.log(req.method);
+
+  if( (/\/websites/).test(req.url) ){
+    if( req.method === 'OPTIONS'){
+      sendResponse(res);
+    }
+
+    else if( req.method === "POST" ) {
+      var dataStore = '';
+      req.on('data', function(data) {
+        dataStore += data;
+      });
+
+      req.on('end', function() {
+        console.log(JSON.parse(dataStore));
+      })
+
+      sendResponse(res, 201);
+    }
+  }
+
+
   res.end(archive.paths.list);
 };
+
+
+// 1. / serve index.html
+// 2, /website
+//www.google.com
+
+
+
